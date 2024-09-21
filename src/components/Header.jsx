7d285e0e7a5499581a84dbd7b1b7d2b982cd2c5d
valyastriz <valyastriz@gmail.com>
2024-09-21 15:36:11 -0400

    added logic to navigation.jsx and bootstrap styling to header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,12 @@ import Navigation from './Navigation';
 
 const Header = () => {
     return (
-        <header>
-            <h1>Valya Strizheus</h1>
-            <Navigation /> {/* This will include the navigation component */}
+        <header className="bg-dark text-white p-3">
+            <div className="container d-flex justify-content-between align-items-center">
+                <h1 className="header-title">Valya Strizheus</h1>
+                <Navigation /> {/* This will include the navigation component */}
+            </div>
+
         </header>
     );
 };
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+    return (
+        <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+            <NavLink className="nav-link" exact to="/">About Me</NavLink>
+            </li>
+            <li className="nav-item">
+            <NavLink className="nav-link" to="/portfolio">Portfolio</NavLink>
+            </li>
+            <li className="nav-item">
+            <NavLink className="nav-link" to="/contact">Contact</NavLink>
+            </li>
+            <li className="nav-item">
+            <NavLink className="nav-link" to="/resume">Resume</NavLink>
+            </li>
+        </ul>
+    );
+};
