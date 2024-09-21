c28774695c406081feebcb7aabcb1cf74a0eee3f
valyastriz <valyastriz@gmail.com>
2024-09-21 18:10:05 -0400

    updated css styles in index.css  for background color

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -11,7 +11,10 @@ const AboutMe = () => {
         <div className="col-md-8">
           <h2>About Me</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Hey there, I'm Valentina Strizheus, a.k.a. "Valya", and I'm diving headfirst into the world of web development! There's nothing quite like the thrill of seeing lines of code come together to create something awesome. Every project is a chance for me to learn and grow, and I'm all about embracing the challenges that come my way.
+          </p>
+          <p>
+          When I'm not busy coding, you'll find me hanging out with my furry companions. I'm definitely a dog person and love to teach my dogs new tricks! It's amazing how their enthusiasm mirrors my own when I'm diving into the latest web development challenges. Just like teaching them to fetch or roll over, learning new tricks in web development is all about patience, practice, and a sprinkle of creativity. So, whether I'm teaching my pups a new trick or tackling a coding problem, I'm all about embracing the journey and enjoying every step of the way.
           </p>
         </div>
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import Navigation from './Navigation';
 
 const Header = () => {
     return (
-        <header className="bg-dark text-white p-3 sticky-top">
-            <div className="container d-flex justify-content-between align-items-center">
+        <header className="bg-dark text-white p-3 sticky-top w-100">
+            <div className="d-flex justify-content-between align-items-center">
                 <h1 className="header-title">Valya Strizheus</h1>
                 <Navigation /> {/* This will include the navigation component */}
             </div>
